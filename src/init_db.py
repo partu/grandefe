c187783987e00ae6fc8c85cde660c5c5e08f5a94
partu18 <artusopablo@gmail.com>
2016-03-06 13:19:12 -0300

    Rfactoring repo

--- src/init_db.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-from dbconnector import DBConnector
-from catalog import DefeCatalog
-
-
-if __name__ == '__main__':
-	catalog = DefeCatalog()
-	connector = DBConnector(catalog.db_name)
-	connector.init_db_with_catalog(catalog)
